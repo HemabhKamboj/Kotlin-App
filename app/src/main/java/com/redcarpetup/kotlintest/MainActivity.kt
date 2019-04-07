3e138bff38ce932c4678bfdb5a6a4a3e3b4853b5
Hemabh Kamboj <hkamboj2603199@gmail.com>
2019-04-07 23:17:23 +0530

    added button and random text display on button click

--- app/src/main/java/com/redcarpetup/kotlintest/MainActivity.kt
+++ app/src/main/java/com/redcarpetup/kotlintest/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.redcarpetup.kotlintest
-
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.redcarpetup.kotlintest.ui.main.MainFragment
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+
+        val buttonCLick = findViewById<Button>(R.id.addBtn)
+
+        val t = findViewById<TextView>(R.id.textView)
+        buttonCLick.setOnClickListener{
+            val list = arrayListOf("text1", "text2", "text3")
+            val random = Random()
+            val randomtext = random.nextInt(list.count())
+
+            t.text=list[randomtext]
+
         }
+
     }
 
 }
